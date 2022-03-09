fa8f37ce6ffdfb024699a8ec54416b4dddc22b52
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-03-09 12:21:43 -0700

    some styling

--- client/src/pages/Dragon.js
+++ client/src/pages/Dragon.js
@@ -4,14 +4,13 @@ import DragonForm from './DragonForm'
 const Dragon = (props) => {
     const {id, variety, color, element, personality, alignment, age, hp, cr, deleteDragon, updateDragon} = props
     return (
-        <div className='box'>
-            <p>Color: {color}</p>
-            <p>Type: {variety}</p>
-            <p>Alignment: {alignment}</p>
+        <div className='dragonContainer'>
+            <h2>{age} {color} Dragon</h2>
+            <h3>{variety} | {alignment}</h3>
             <p>Breath Weapon/s: {element}</p>
-            <p>Personality: {personality}</p>
-            <p>Age: {age}</p>
-            <p>HP: {hp} | CR: {cr}</p>
+            <p><b>Personality:</b></p> 
+            <p>{personality}</p>
+            <p><b>HP:</b> {hp} | <b>CR:</b> {cr}</p>
             <p>ID: {id}</p>
             <button onClick={() => deleteDragon(id)}>Delete Dragon</button>
             <DragonForm id={id} color={color} updateDragon={updateDragon} />
--- client/src/pages/DragonForm.js
+++ client/src/pages/DragonForm.js
@@ -21,14 +21,14 @@ const DragonForm = (props) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <p>Color: <input value={color} onChange={(e) => setColor(e.target.value)} /></p>
-            <p>Variety: <input value={variety} onChange={(e) => setVariety(e.target.value)} /></p>
-            <p>Element: <input value={element} onChange={(e) => setElement(e.target.value)} /></p>
-            <p>Personality: <input value={personality} onChange={(e) => setPersonality(e.target.value)} /></p>         
-            <p>Alignment: <input value={alignment} onChange={(e) => setAlignment(e.target.value)} /></p>       
-            <p>Age: <input value={age} onChange={(e) => setAge(e.target.value)} /></p>       
-            <p>HP: <input value={hp} onChange={(e) => setHp(e.target.value)} /></p> 
-            <p>CR: <input value={cr} onChange={(e) => setCr(e.target.value)} /></p>
+            <p className='formInput'>Color: <input value={color} onChange={(e) => setColor(e.target.value)} /></p>
+            <p className='formInput'>Variety: <input value={variety} onChange={(e) => setVariety(e.target.value)} /></p>
+            <p className='formInput'>Element: <input value={element} onChange={(e) => setElement(e.target.value)} /></p>
+            <p className='formInput'>Personality: <input value={personality} onChange={(e) => setPersonality(e.target.value)} /></p>         
+            <p className='formInput'>Alignment: <input value={alignment} onChange={(e) => setAlignment(e.target.value)} /></p>       
+            <p className='formInput'>Age: <input value={age} onChange={(e) => setAge(e.target.value)} /></p>       
+            <p className='formInput'>HP: <input value={hp} onChange={(e) => setHp(e.target.value)} /></p> 
+            <p className='formInput'>CR: <input value={cr} onChange={(e) => setCr(e.target.value)} /></p>
             <hr />
             <button>{props.id ? 'Update' : 'New'}</button>
         </form>
